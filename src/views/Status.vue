950b4ada22f4c9112e174e6a1a988c951eb9b5cd
benjamin <benjaminchen.c@gmail.com>
2021-03-04 05:05:59 +0800

    update status

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -11,7 +11,7 @@
                   <img src="/images/heart.gif" alt="" width="150">
                 </div>
                 <div class="col-4 my-auto">
-                  <h3>89</h3>
+                  <h3>{{ heartRate }}</h3>
                 </div>
                 <div class="col-4 my-auto">
                   <span>Hear Rate</span>
@@ -30,7 +30,7 @@
                   <img src="/images/blood.gif" alt="" width="150">
                 </div>
                 <div class="col-4 my-auto">
-                  <h3>108/68</h3>
+                  <h3>{{ bpH }}/{{ bpL }}</h3>
                 </div>
                 <div class="col-4 my-auto">
                   <span>Blood Pressure</span>
@@ -47,7 +47,7 @@
               <div class="row">
                 <div class="col-4"></div>
                 <div class="col-4 my-auto">
-                  <h3>36.7</h3>
+                  <h3>{{ temperature }}</h3>
                 </div>
                 <div class="col-4 my-auto">
                   <span>Temperature</span>
@@ -74,13 +74,55 @@
           </div>
         </div>
       </b-list-group-item>
+      <b-list-group-item>
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-6 mx-auto">
+              <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4 my-auto">
+                  <h3>{{ steps }}</h3>
+                </div>
+                <div class="col-4 my-auto">
+                  <span>Steps</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </b-list-group-item>
     </b-list-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Status'
+  name: 'Status',
+  mounted: function() {
+    this.intervalId = setInterval(() => this.random(), 1000);
+  },
+  beforeDestroy: function() {
+    clearInterval(this.intervalId)
+  },
+  data: () => {
+    return {
+      intervalId: null,
+      heartRate: 72,
+      bpH: 109,
+      bpL: 88,
+      temperature: 36.6,
+      steps: 3096
+    }
+  },
+  methods: {
+    random() {
+      this.heartRate = 70 + Math.floor(Math.random() * 10)
+      this.bpH = 100 + Math.floor(Math.random() * 10)
+      this.bpL = 65 + Math.floor(Math.random() * 10)
+      this.temperature = 36 + Math.floor(Math.random())
+      this.steps++
+    }
+  }
 }
 </script>
 
