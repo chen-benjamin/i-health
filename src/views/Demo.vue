a660274d9fd16260f92783c1798343406b620ed8
benjamin <benjaminchen.c@gmail.com>
2021-03-03 22:53:20 +0800

    add if and for demo

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -6,11 +6,27 @@
             <b-button variant="primary" href="#">More Info</b-button>
         </b-jumbotron>
     </div>
+    <h1 v-if="isActive">If statement demo</h1>
+    <b-list-group>
+      <b-list-group-item v-for="user, index in users" :key="index">{{ user.name }}</b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Demo'
+  name: 'Demo',
+  data() {
+    return {
+      isActive: true,
+      users: [
+        {id: 1, name: 'a'},
+        {id: 2, name: 'b'},
+        {id: 3, name: 'c'},
+        {id: 4, name: 'd'},
+        {id: 5, name: 'e'}
+      ]
+    }
+  }
 }
 </script>
